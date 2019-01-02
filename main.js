f8e19ab9bce4959f48e47b3be585cd8f48e5fd1d
Beata Markunas <beata.markunas@gmail.com>
2019-01-02 22:07:37 +0100

    Create if statement.

--- main.js
+++ main.js
@@ -4,4 +4,20 @@ let blue = 100;
 
 document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
 
+const changeColor = (e) => {
+    console.log(e.keyCode, e.which);
+    if (e.keyCode === 38 && red < 256) {
+        red++;
+        green++;
+        blue++;
+        document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+
+    } else if (e.keyCode === 40 && red >= 0) {
+        red--;
+        green--;
+        blue--;
+        document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    }
+}
+
 window.addEventListener('keydown', changeColor)
