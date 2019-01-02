a94e328a981e47df12a57017349b5c4096ca2af8
Beata Markunas <beata.markunas@gmail.com>
2019-01-02 22:22:14 +0100

    Create switch statement.

--- main.js
+++ main.js
@@ -6,18 +6,31 @@ document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
 
 const changeColor = (e) => {
     console.log(e.keyCode, e.which);
-    if (e.keyCode === 38 && red < 256) {
-        red++;
-        green++;
-        blue++;
-        document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    // IF STATEMENT
 
-    } else if (e.keyCode === 40 && red >= 0) {
-        red--;
-        green--;
-        blue--;
-        document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    // if (e.keyCode === 38 && red < 256) {
+    //     red++;
+    //     green++;
+    //     blue++;
+    //     document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+
+    // } else if (e.keyCode === 40 && red >= 0) {
+    //     red--;
+    //     green--;
+    //     blue--;
+    //     document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    // }
+
+    // SWITCH
+    switch (e.keyCode) {
+        case 38:
+            document.body.style.backgroundColor = `rgb(${red < 255 ? red++ : red},${green < 255 ? green++ : green},${blue < 255 ? blue++: blue})`;
+            break;
+        case 40:
+            document.body.style.backgroundColor = `rgb(${red > 0 ? red-- : red},${green > 0 ? green-- : green},${blue > 0 ? blue-- : blue})`;
+            break;
     }
+
 }
 
 window.addEventListener('keydown', changeColor)
